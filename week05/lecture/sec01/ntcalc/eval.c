d2cd813cfe94480d9d6579f4e600b4cf08f01a38
Greg Benson <benson@usfca.edu>
2020-09-15 09:50:44 -0700

    Added week05 sec01 ntcalc with lab04 solution.

---
+++ week05/lecture/sec01/ntcalc/eval.c
@@ -0,0 +1,33 @@
+/* eval.c - tree interpretation */
+
+#include "ntcalc.h"
+uint32_t eval_tree(struct parse_node_st *np) {
+    uint32_t v1, v2;
+    
+    if (np->type == EX_INTVAL) {
+        return np->intval.value;
+    } else if (np->type == EX_OPER1) {
+        v1 = eval_tree(np->oper1.operand);
+        if (np->oper1.oper == OP_MINUS) {
+            return -v1;
+        } else if (np->oper1.oper == OP_NOT) {
+            return ~v1;
+        }
+    } else if (np->type == EX_OPER2) {
+        v1 = eval_tree(np->oper2.left);
+        v2 = eval_tree(np->oper2.right);
+        if (np->oper2.oper == OP_PLUS) {
+            return v1 + v2;
+        } else if (np->oper2.oper == OP_MINUS) {
+            return v1 - v2;
+        } else if (np->oper2.oper == OP_MULT) {
+            return v1 * v2;
+        } else if (np->oper2.oper == OP_DIV) {
+            return v1 / v2;
+        } else if (np->oper2.oper == OP_LSL) {
+            return v1 << v2;
+        }
+
+
+    }
+}
---
+++ week05/lecture/sec01/ntcalc/ntcalc.h
@@ -0,0 +1,142 @@
+/* ntcalc.h */
+
+#ifndef _NTCALC_H
+#define _NTCALC_H
+
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+/*
+ * scan.c
+ */
+
+/*
+
+# Scanner EBNF
+
+tokenlist   ::= (token)*
+token       ::= intlit | binlit | symbol
+integer     ::= digit (digit)*
+symbol      ::= '+' | '-' | '*' | '/' | '(' | ')' | '~' | '<<'
+digit       ::= '0' | ... | '9'
+binlit      ::= '0b' ['0', '1'] (['0', '1'])*
+# Ignore
+
+whitespace  ::=  (' ' | '\t') (' ' | '\t')* 
+
+*/
+
+#define SCAN_TOKEN_LEN 32
+#define SCAN_TOKEN_TABLE_LEN 1024
+#define SCAN_INPUT_LEN 2048
+
+enum scan_token_enum {
+    TK_INTLIT, /* -123, 415 */
+    TK_BINLIT, /* 0b1010, 0b00001111 */
+    TK_LPAREN, /* ( */
+    TK_RPAREN, /* ) */
+    TK_PLUS,   /* + */
+    TK_MINUS,  /* - */
+    TK_MULT,   /* * */
+    TK_DIV,    /* / */
+    TK_NOT,    /* ~ */
+    TK_LSL,    /* << */
+    TK_EOT,    /* end of text */
+    TK_ANY,    /* wildcard for parsing */
+};
+
+#define SCAN_TOKEN_STRINGS {"TK_INTLIT", "TK_BINLIT", \
+                            "TK_LPAREN", "TK_RPAREN", \
+                            "TK_PLUS", "TK_MINUS", "TK_MULT", "TK_DIV", \
+                            "TK_NOT", "TK_LSL", "TK_EOT", "TK_ANY"};
+
+struct scan_token_st {
+    enum scan_token_enum id;
+    char value[SCAN_TOKEN_LEN];
+};
+
+struct scan_table_st {
+    struct scan_token_st table[SCAN_TOKEN_TABLE_LEN];
+    int len;
+    int next;
+};
+
+void scan_table_init(struct scan_table_st *st);
+void scan_table_scan(struct scan_table_st *st, char *input, int len);
+void scan_table_print(struct scan_table_st *st);
+struct scan_token_st * scan_table_get(struct scan_table_st *st, int i);
+bool scan_table_accept(struct scan_table_st *st, enum scan_token_enum tk_expected);
+
+/*
+ * parse.c
+ */
+
+ /*
+ A simple grammar for the ntcalc langauge
+
+ # Parser
+
+ program    ::= expression EOT
+ 
+ expression ::= operand (operator operand)*
+
+ operand    ::= intlit
+              | binlit
+              | '-' operand
+              | '~' operand
+              | '(' expression ')'
+*/
+
+enum parse_expr_enum {EX_INTVAL, EX_OPER1, EX_OPER2};
+enum parse_oper_enum {OP_PLUS, OP_MINUS, OP_MULT, OP_DIV,
+                      OP_NOT, OP_LSL};
+
+struct parse_node_st {
+    enum parse_expr_enum type;
+    union {
+        struct {uint32_t value;} intval;
+        struct {enum parse_oper_enum oper;
+                struct parse_node_st *operand;} oper1;
+        struct {enum parse_oper_enum oper;
+                struct parse_node_st *left;
+                struct parse_node_st *right;} oper2;
+    };                
+};
+
+#define PARSE_TABLE_LEN 1024
+
+struct parse_table_st {
+    struct parse_node_st table[PARSE_TABLE_LEN];
+    int len;
+};
+
+void parse_table_init(struct parse_table_st *pt);
+struct parse_node_st * parse_node_new(struct parse_table_st *pt);
+void parse_tree_print(struct parse_node_st *np);
+struct parse_node_st * parse_program(struct parse_table_st *pt, 
+                                     struct scan_table_st *st);
+
+/*
+ * conv.c
+ */
+
+ uint32_t conv_binstr_to_uint32(char *binstr);
+ void conv_uint32_to_binstr(uint32_t v, char *binstr);
+
+
+/*
+ * eval.c
+ */
+
+ uint32_t eval_tree(struct parse_node_st *np);
+
+
+#endif /* _NTCALC_H */
