3168b4ed3e820f11bd41d3c64b8c32c67fd714e2
Greg Benson <benson@usfca.edu>
2020-09-15 20:59:30 -0700

    Pushed week05 lab04 code.

---
+++ week05/lecture/sec02/ntcalc/ntcalc.c
@@ -0,0 +1,97 @@
+/* ntcalc.c */
+
+#include "ntcalc.h"
+
+struct config_st {
+    char input[SCAN_INPUT_LEN];
+    int base;
+    bool debug;
+};
+
+void ntcalc_config_init(struct config_st *cp) {
+    cp->input[0] = '\0';
+    cp->base = 10;
+}
+
+void ntcalc_print_usage (void) {
+    printf("usage: ./ntcalc [-b base] -e \"expr\"\n");
+    printf("  -b base can be 2 or 10\n");
+    printf("  examples: ./ntcalc -e \"1 + 2\"\n");
+    printf("  examples: ./ntcalc -b 2 -e \"~(0b1110 >> 2)\"\n");  
+}
+
+void ntcalc_parse_args(int argc, char **argv, struct config_st *cp) {
+    int i = 1;
+
+    if (argc == 1) {
+        ntcalc_print_usage();
+        exit(0);
+    }
+
+    while (i < argc) {
+        if (argv[i][0] == '-' && argv[i][1] == 'e') {
+            strncpy(cp->input, argv[i+1], SCAN_INPUT_LEN);
+            i += 2;
+        } else if (argv[i][0] == '-' && argv[i][1] == 'b') {
+            cp->base = atoi(argv[i+1]);
+            i += 2;
+        } else if (argv[i][0] == '-' && argv[i][1] == 'd') {
+            cp->debug = true;
+            i += 1;
+        } else {
+            printf("Invalid option: %s\n", argv[i]);
+            exit(-1);
+        }
+    }
+
+    if (!((cp->base == 2) || (cp->base == 10))) {
+        printf("Invalid base\n");
+        ntcalc_print_usage();
+        exit(-1);
+    }
+
+    if (strnlen(cp->input, SCAN_INPUT_LEN) == 0) {
+        printf("No expression provided\n");
+        ntcalc_print_usage();
+        exit(-1);
+    }
+}
+
+int main(int argc, char **argv) {
+
+    /* scanning with new simplified ntcalc scanner */
+    struct scan_table_st scan_table;
+    struct parse_table_st parse_table;
+    struct parse_node_st *parse_tree;
+    int len;
+    uint32_t result;
+    struct config_st config;
+    char result_str[SCAN_INPUT_LEN];
+
+    ntcalc_config_init(&config);
+    ntcalc_parse_args(argc, argv, &config);
+
+    len = strnlen(config.input, SCAN_INPUT_LEN);
+
+    scan_table_init(&scan_table);
+    scan_table_scan(&scan_table, config.input, len);
+    if (config.debug) {
+        scan_table_print(&scan_table);
+    }
+
+    parse_table_init(&parse_table);
+    parse_tree = parse_program(&parse_table, &scan_table);
+    if (config.debug) {
+        parse_tree_print(parse_tree);
+    }
+
+    result = eval_tree(parse_tree);
+    if (config.base == 2) {
+        conv_uint32_to_binstr(result, result_str);
+        printf("%s\n", result_str);
+    } else if (config.base == 10) {
+        printf("%d\n", result);
+    }
+
+    return 0;
+}
--- week05/lecture/sec02/ntcalc/ntcalc.h
+++ week05/lecture/sec02/ntcalc/ntcalc.h
@@ -28,6 +28,7 @@ intlit      ::= digit (digit)*
 symbol      ::= '+' | '-' | '*' | '/' | '(' | ')' | '~' | '<<'
 digit       ::= '0' | ... | '9'
 binlit      ::= '0b' ['0', '1'] (['0', '1'])*
+
 # Ignore
 
 whitespace  ::=  (' ' | '\t') (' ' | '\t')* 
@@ -40,7 +41,7 @@ whitespace  ::=  (' ' | '\t') (' ' | '\t')*
 
 enum scan_token_enum {
     TK_INTLIT, /* -123, 415 */
-    TK_BINLIT, /* 0b1010, 0b00001111 */
+    TK_BINLIT, /* 0b1010, 0b10001111 */
     TK_LPAREN, /* ( */
     TK_RPAREN, /* ) */
     TK_PLUS,   /* + */
@@ -56,7 +57,8 @@ enum scan_token_enum {
 #define SCAN_TOKEN_STRINGS {"TK_INTLIT", "TK_BINLIT", \
                             "TK_LPAREN", "TK_RPAREN", \
                             "TK_PLUS", "TK_MINUS", "TK_MULT", "TK_DIV", \
-                            "TK_NOT", "TK_LSL", "TK_EOT", "TK_ANY"};
+                            "TK_NOT", "TK_LSL", \
+                            "TK_EOT", "TK_ANY"};
 
 struct scan_token_st {
     enum scan_token_enum id;
@@ -88,16 +90,14 @@ bool scan_table_accept(struct scan_table_st *st, enum scan_token_enum tk_expecte
  
  expression ::= operand (operator operand)*
 
- operand    ::= intlit
-              | binlit
+ operand    ::= integer
               | '-' operand
               | '~' operand
               | '(' expression ')'
 */
 
 enum parse_expr_enum {EX_INTVAL, EX_OPER1, EX_OPER2};
-enum parse_oper_enum {OP_PLUS, OP_MINUS, OP_MULT, OP_DIV,
-                      OP_NOT, OP_LSL};
+enum parse_oper_enum {OP_PLUS, OP_MINUS, OP_MULT, OP_DIV, OP_NOT, OP_LSL};
 
 struct parse_node_st {
     enum parse_expr_enum type;
@@ -131,12 +131,10 @@ struct parse_node_st * parse_program(struct parse_table_st *pt,
  uint32_t conv_binstr_to_uint32(char *binstr);
  void conv_uint32_to_binstr(uint32_t v, char *binstr);
 
-
 /*
  * eval.c
  */
 
  uint32_t eval_tree(struct parse_node_st *np);
-
 
 #endif /* _NTCALC_H */
