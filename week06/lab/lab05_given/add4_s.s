762b15ed7360c4f80ded526872f1549c18d84c32
Greg Benson <benson@usfca.edu>
2020-09-25 10:51:49 -0700

    Fixed comments in given code.

--- week06/lab/lab05_given/add4_s.s
+++ week06/lab/lab05_given/add4_s.s
@@ -2,7 +2,7 @@
 
 .global add4_s
 
-/* a0 is r0, a1 is r1, a2 is r3, a3 is r4 */
+/* a0 is r0, a1 is r1, a2 is r2, a3 is r3 */
 add4_s:
     mov r0, #0 /* DELETE this line and add correct instructions. */
     bx lr
--- week06/lab/lab05_given/quadratic_s.s
+++ week06/lab/lab05_given/quadratic_s.s
@@ -2,7 +2,7 @@
 
 .global quadratic_s
 
-/* x is r0, a is r1, b is r3, c is r4 */
+/* x is r0, a is r1, b is r2, c is r3 */
 quadratic_s:
     mov r0, #0 /* DELETE this line and add correct instructions. */
     bx lr
